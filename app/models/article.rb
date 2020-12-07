4a196a1e1a01146d1fdc4c55aa9b50287280024e
Y <>
2020-12-07 20:10:01 +0900

    正規表現

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,9 +11,11 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 100}
+  validates :title, format: { with: /\A(?!\@)/ }
   
   validates :content, presence: true
   validates :content, length: { minimum: 10 }
+  validates :content, uniqueness: true
 
   def display_created_at
      I18n.l(self.created_at, format: :default)
