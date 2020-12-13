73d919890a09708da23de37cb1484f96929228f4
Y <>
2020-12-13 17:23:42 +0900

    自分の記事に対してアクションボタンを表示するように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy       
+  
+  def has whitten?(article)
+    articles.exists?(id: article.id)   
+  end
 end
+
