229df87f7da7fb43c5d9be4d6a9b83ee5d5ee477
Y <>
2021-01-20 18:18:15 +0900

    action text の設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -21,10 +21,6 @@ class Article < ApplicationRecord
   validates :title, format: { with: /\A(?!\@)/ }
 
   validates :content, presence: true
-  validates :content, length: { minimum: 10 }
-  validates :content, uniqueness: true
-
-  validate :validate_title_and_content_length
 
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
@@ -43,8 +39,4 @@ class Article < ApplicationRecord
   end
 
   private
-  def validate_title_and_content_length
-   char_count =  self.title.length + self.content.length
-   errors.add(:content, '100文字以上で！') unless char_count > 100
-  end
 end
